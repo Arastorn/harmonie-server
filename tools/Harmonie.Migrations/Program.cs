71f895a3eb7b5382c42cfae489d15e13988356e7
Antoine Bourgeois <pro.abourgeois@gmail.com>
2026-02-19 01:38:54 +0100

    fix migrations inputs

--- tools/Harmonie.Migrations/Program.cs
+++ tools/Harmonie.Migrations/Program.cs
@@ -4,7 +4,8 @@ using System.Reflection;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
     .Build();
 
 var connectionString = configuration.GetConnectionString("DefaultConnection");
